0db9b0c8ff00de08da08bdac851ad6a0627aef32
Rafael Neves <rafa.mneves@hotmail.com>
2022-04-24 23:26:34 -0300

    add products to cart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
         <div class="product-image" :style="{ backgroundImage: 'url(' + product.image + ')' }"></div>
         <h4>{{ product.title }}</h4>
         <p class="price">US$ {{ product.price.toFixed(2) }}</p>
-        <button>Add to bag</button>
+        <button @click="addToBag(product)">Add to bag</button>
       </div>
     </div>
   </div>
@@ -22,9 +22,17 @@ export default {
     products() {
       return this.$store.state.products;
     },
+    productsInBag() {
+      return this.$store.state.productsInBag;
+    },
   },
 
-  methods: {},
+  methods: {
+    addToBag(product) {
+      product.quantity = 1;
+      this.$store.dispatch('addToBag', product);
+    },
+  },
 };
 </script>
 
